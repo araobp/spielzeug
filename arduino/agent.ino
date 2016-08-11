3f7fc1a236bb2fdcce58f7423724e67488ba5c3f
arao <araobp@users.noreply.github.com>
2016-08-12 04:36:21 +0900

    Update agent.ino

--- arduino/agent.ino
+++ arduino/agent.ino
@@ -2,7 +2,7 @@
 
 #include<Servo.h>
 
-//#define LED_PIN 13
+#define LED_PIN 13
 
 // sensor/actuator ID
 #define LED 0
@@ -11,9 +11,16 @@
 #define PROXIMITY_SENSOR 3
 
 // Digital
-#define PIN_SERVO 9     // PWM: Tower Pro SG90
-#define PIN_TRIGGER 13  // Sain Smart HC-SR04
-#define PIN_ECHO 12     // Sain Smart HC-SR04
+// Note: PWM on pin 9,10 and 11 does not seem to work after servo.attach() is called. 
+#define PIN_MOTOR2_IN2 2  // TA7291P IN2
+#define PIN_MOTOR2_IN1 4  // TA7291P IN1
+#define PIN_MOTOR2_VREF 5  // PWM: TA7291P VREF
+#define PIN_MOTOR1_IN2 7  // TA7291P IN2
+#define PIN_MOTOR1_IN1 8  // TA7291P IN1
+#define PIN_MOTOR1_VREF 6  // PWM: TA7291P VREF
+#define PIN_SERVO 11  // PWM: Tower Pro SG90
+#define PIN_ECHO 12  // Sain Smart HC-SR04
+#define PIN_TRIGGER 10  // Sain Smart HC-SR04
 
 // Analog in
 #define PIN_OPTICAL1 0  // Sharp GP2Y0AAA21YK0F
@@ -23,6 +30,8 @@
 #define ULTRASONIC 0
 #define OPTICAL1 1
 #define OPTICAL2 2
+#define MOTOR1 1
+#define MOTOR2 2
 
 // Constants
 #define C_A 32.0
@@ -31,10 +40,20 @@
 // Servo motor class
 Servo servo;
 
+// TA7291P direction control
+int in1 = HIGH;
+int in2 = LOW;
+
 void setup(){
-  //pinMode(LED_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
   pinMode(PIN_TRIGGER, OUTPUT);
   pinMode(PIN_ECHO, INPUT);
+  pinMode(PIN_MOTOR1_IN1, OUTPUT);
+  pinMode(PIN_MOTOR1_IN2, OUTPUT);
+  pinMode(PIN_MOTOR1_VREF, OUTPUT);
+  pinMode(PIN_MOTOR2_IN1, OUTPUT);
+  pinMode(PIN_MOTOR2_IN2, OUTPUT);
+  pinMode(PIN_MOTOR2_VREF, OUTPUT);
   servo.attach(PIN_SERVO);
   Serial.begin(9600);
 }
@@ -91,20 +110,44 @@ void loop(){
     } else {
       String sign_s = cmd.substring(3,4);
       int sign = 1;
-      if (sign_s == "0") {
+      if (sign_s == "1") {
         sign = -1;
       }
       int value = cmd.substring(4,7).toInt();
       switch(device) {
-//        case LED:
-//          digitalWrite(LED_PIN, value);
-//          Serial.println("0");  // OK
-//          break;
+        case LED:
+          if (sign > 0) {
+            digitalWrite(LED_PIN, HIGH);
+          } else {
+            digitalWrite(LED_PIN, LOW);
+          }
+          Serial.println("0");  // OK
+          break;
         case MOTOR:
-          //Serial.println("motor");
-          //Serial.println(unit);
-          //TODO: motor control
-          Serial.println(0);  // OK
+          if (sign > 0) {
+            in1 = HIGH;
+            in2 = LOW;
+          } else {
+            in1 = LOW;
+            in2 = HIGH;
+          }
+          if (value > 255) {
+            Serial.println(-1);
+            break;
+          }
+          switch(unit) {
+            case MOTOR1:
+              digitalWrite(PIN_MOTOR1_IN1, in1);
+              digitalWrite(PIN_MOTOR1_IN2, in2);
+              analogWrite(PIN_MOTOR1_VREF, value);
+              break;
+            case MOTOR2:
+              digitalWrite(PIN_MOTOR2_IN1, in1);
+              digitalWrite(PIN_MOTOR2_IN2, in2);
+              analogWrite(PIN_MOTOR2_VREF, value);
+              break;
+          }
+          Serial.println(0);
           break;
         case SERVO_MOTOR:
           // from -90 degrees to +90 degrees
