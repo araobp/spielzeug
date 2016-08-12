f5c3474373851ffff95a6718d5fef80c5a4fec63
arao <araobp@users.noreply.github.com>
2016-08-13 04:01:26 +0900

    Update agent.ino

--- arduino/agent.ino
+++ arduino/agent.ino
@@ -70,94 +70,105 @@ int calc_dist(int pin) {
 }
 
 void loop(){
-  String cmd = Serial.readStringUntil('\n');
-  if (cmd != NULL) {
-    cmd.trim();
-    //Serial.println(cmd);
-    int rw = cmd.substring(0,1).toInt();
-    int device = cmd.substring(1,2).toInt();
-    int unit = cmd.substring(2,3).toInt();
-    //Serial.println(rw);
-    //Serial.println(device);
-    if (rw == 0) {
-      switch(device) {
-        case PROXIMITY_SENSOR:
-          switch(unit) {
-            case ULTRASONIC:
-              long duration, distance;
-              digitalWrite(PIN_TRIGGER, LOW);
-              delayMicroseconds(2);
-              digitalWrite(PIN_TRIGGER, HIGH);
-              delayMicroseconds(10);
-              digitalWrite(PIN_TRIGGER, LOW);
-              duration = pulseIn(PIN_ECHO, HIGH);
-              distance = (duration/2)/29.1;
-              if (distance < 2 || distance > 180) {
-                Serial.println(-1);  // Out of range 
-              } else {
-                Serial.println(distance);
-              }
-              break;
-            case OPTICAL1:
-              Serial.println(calc_dist(PIN_OPTICAL1));
-              break;
-            case OPTICAL2:
-              Serial.println(calc_dist(PIN_OPTICAL2));
-              break;
-          }
-          break;
-      }
-    } else {
-      String sign_s = cmd.substring(3,4);
-      int sign = 1;
-      if (sign_s == "1") {
-        sign = -1;
-      }
-      int value = cmd.substring(4,7).toInt();
-      switch(device) {
-        case LED:
-          if (sign > 0) {
-            digitalWrite(LED_PIN, HIGH);
-          } else {
-            digitalWrite(LED_PIN, LOW);
-          }
-          Serial.println("0");  // OK
-          break;
-        case MOTOR:
-          if (sign > 0) {
-            in1 = HIGH;
-            in2 = LOW;
-          } else {
-            in1 = LOW;
-            in2 = HIGH;
-          }
-          if (value > 255) {
-            Serial.println(-1);
+  if (Serial.available() > 0) {
+    String cmd = Serial.readStringUntil('\n');
+    if (cmd != NULL) {
+      cmd.trim();
+      //Serial.println(cmd);
+      int rw = cmd.substring(0,1).toInt();
+      int device = cmd.substring(1,2).toInt();
+      int unit = cmd.substring(2,3).toInt();
+      //Serial.println(rw);
+      //Serial.println(device);
+      if (rw == 0) {
+        switch(device) {
+          case PROXIMITY_SENSOR:
+            switch(unit) {
+              case ULTRASONIC:
+                long duration, distance;
+                digitalWrite(PIN_TRIGGER, LOW);
+                delayMicroseconds(2);
+                digitalWrite(PIN_TRIGGER, HIGH);
+                delayMicroseconds(10);
+                digitalWrite(PIN_TRIGGER, LOW);
+                duration = pulseIn(PIN_ECHO, HIGH);
+                distance = (duration/2)/29.1;
+                if (distance < 2 || distance > 180) {
+                  Serial.println(-1);  // Out of range 
+                } else {
+                  Serial.println(distance);
+                }
+                break;
+              case OPTICAL1:
+                Serial.println(calc_dist(PIN_OPTICAL1));
+                break;
+              case OPTICAL2:
+                Serial.println(calc_dist(PIN_OPTICAL2));
+                break;
+            }
             break;
-          }
-          switch(unit) {
-            case MOTOR1:
-              digitalWrite(PIN_MOTOR1_IN1, in1);
-              digitalWrite(PIN_MOTOR1_IN2, in2);
-              analogWrite(PIN_MOTOR1_VREF, value);
+        }
+      } else {
+        String sign_s = cmd.substring(3,4);
+        int sign = 1;
+        if (sign_s == "1") {
+          sign = -1;
+        } else if (sign_s == "2") {
+          sign = 0;
+        }
+        int value = cmd.substring(4,7).toInt();
+        switch(device) {
+          case LED:
+            if (sign > 0) {
+              digitalWrite(LED_PIN, HIGH);
+            } else {
+              digitalWrite(LED_PIN, LOW);
+            }
+            Serial.println("0");  // OK
+            break;
+          case MOTOR:  // Toshiba TA7291P
+            switch(sign) {
+              case 1:  // forward
+                in1 = HIGH;
+                in2 = LOW;
+                break;
+              case -1:  // reverse
+                in1 = LOW;
+                in2 = HIGH;
+                break;
+              case 0:  // break
+                in1 = HIGH;
+                in2 = HIGH;
+                break;
+            }
+            if (value > 150) {
+              Serial.println(-1);  // Vref > Vs(3V)
               break;
-            case MOTOR2:
-              digitalWrite(PIN_MOTOR2_IN1, in1);
-              digitalWrite(PIN_MOTOR2_IN2, in2);
-              analogWrite(PIN_MOTOR2_VREF, value);
-              break;
-          }
-          Serial.println(0);
-          break;
-        case SERVO_MOTOR:
-          // from -90 degrees to +90 degrees
-          if (value > 90) {
-            Serial.println(-1);
-            break; 
-          }
-          servo.write(sign * value + 90);
-          Serial.println(0);
-          break;
+            }
+            switch(unit) {
+              case MOTOR1:
+                digitalWrite(PIN_MOTOR1_IN1, in1);
+                digitalWrite(PIN_MOTOR1_IN2, in2);
+                analogWrite(PIN_MOTOR1_VREF, value);
+                break;
+              case MOTOR2:
+                digitalWrite(PIN_MOTOR2_IN1, in1);
+                digitalWrite(PIN_MOTOR2_IN2, in2);
+                analogWrite(PIN_MOTOR2_VREF, value);
+                break;
+            }
+            Serial.println(0);
+            break;
+          case SERVO_MOTOR:
+            // from -90 degrees to +90 degrees
+            if (value > 90) {
+              Serial.println(-1);
+              break; 
+            }
+            servo.write(sign * value + 90);
+            Serial.println(0);
+            break;
+        }
       }
     }
   }
