d1b81022d42d1e83f4b6064d91dae3f798e021c3
arao <araobp@users.noreply.github.com>
2016-11-26 08:25:53 +0900

    Create i2c.h

---
+++ misc/esp_i2c/i2c.h
@@ -0,0 +1,21 @@
+// I2C read/write func
+
+#include <Wire.h>
+
+byte i2c_read(byte device_address, byte register_address)
+{
+  byte data;
+  Wire.beginTransmission(device_address);
+  Wire.write(register_address);
+  Wire.endTransmission(false);
+  Wire.requestFrom(device_address, 1);
+  return Wire.read();
+}
+
+void i2c_write(byte device_address, byte register_address, byte data)
+{
+  Wire.beginTransmission(device_address);
+  Wire.write(register_address);
+  Wire.write(data);
+  Wire.endTransmission(false);
+}
