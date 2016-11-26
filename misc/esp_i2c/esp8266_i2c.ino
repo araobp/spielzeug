badcff7bf6420ae778c2a82bcba6077b142b0f14
arao <araobp@users.noreply.github.com>
2016-11-26 15:05:36 +0900

    Update esp8266_i2c.ino

--- misc/esp_i2c/esp8266_i2c.ino
+++ misc/esp_i2c/esp8266_i2c.ino
@@ -1,11 +1,18 @@
 #include "i2c.h"
 
-// I2C devices
-#include "l3gd20.h"  // Gyro/temperature sensor L3GD20
+// I2C device: HMC5883L
 #include "hmc5883l.h"  // Geomagnetic sensor HMC5883L
+#define X_MAX 263
+#define X_MIN -326
+#define Y_MAX 55
+#define Y_MIN -537
+
+// I2C device: L3GD20
+#include "l3gd20.h"  // Gyro/temperature sensor L3GD20
 
 void setup() 
 {
+  set_constants(X_MAX, X_MIN, Y_MAX, Y_MIN);
   Wire.begin();  // ESP8266: IO4 SDA, IO5 SCL
   Serial.begin(9600);
   delay(50);
@@ -24,9 +31,9 @@ void loop()
   int16_t z = get_motion('z');
   Serial.println(t);
   Serial.println(m);
-  Serial.println(x);
-  Serial.println(y);
-  Serial.println(z);
+  //Serial.println(x);
+  //Serial.println(y);
+  //Serial.println(z);
   Serial.println("---");
   delay(5000);
 }
