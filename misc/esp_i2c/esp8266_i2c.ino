c583658a3de2977a1177406da0e81292ca689af8
arao <araobp@users.noreply.github.com>
2016-11-27 08:54:29 +0900

    Update esp8266_i2c.ino

--- misc/esp_i2c/esp8266_i2c.ino
+++ misc/esp_i2c/esp8266_i2c.ino
@@ -15,25 +15,24 @@ void setup()
   set_constants(X_MAX, X_MIN, Y_MAX, Y_MIN);
   Wire.begin();  // ESP8266: IO4 SDA, IO5 SCL
   Serial.begin(9600);
-  delay(50);
+  delay(1000);
   init_hmc5883l;  // initialize HMC5883L
-  delay(50);
   init_l3gd20();  // initialize L3GD20
-  delay(50);
 }
 
 void loop() 
 {
   int8_t t = get_temp();
-  double m = get_radian();
+  double r = get_radian();
   int16_t x = get_motion('x');
   int16_t y = get_motion('y');
   int16_t z = get_motion('z');
+  Serial.println(r);
+  Serial.println("-");
   Serial.println(t);
-  Serial.println(m);
-  //Serial.println(x);
-  //Serial.println(y);
-  //Serial.println(z);
+  Serial.println(x);
+  Serial.println(y);
+  Serial.println(z);
   Serial.println("---");
   delay(5000);
 }
