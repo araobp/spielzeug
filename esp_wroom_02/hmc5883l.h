2a450fad2ba30acc9d99f3257bc2e8acabc6ca35
arao <araobp@users.noreply.github.com>
2016-11-30 03:58:47 +0900

    Update hmc5883l.h

--- esp_wroom_02/hmc5883l.h
+++ esp_wroom_02/hmc5883l.h
@@ -6,6 +6,8 @@ void set_constants(int16_t x_max, int16_t x_min, int16_t y_max, int16_t y_min);
 
 void init_hmc5883l();
 
-double get_radian(void);
+double get_radian();
+
+int16_t get_degree();
 
 #endif
