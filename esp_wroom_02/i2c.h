ecac157f85f77d3990a90a0f3a32eb8313e91be1
arao <araobp@users.noreply.github.com>
2016-11-29 21:07:25 +0900

    Update i2c.h

--- esp_wroom_02/i2c.h
+++ esp_wroom_02/i2c.h
@@ -1,21 +1,9 @@
-// I2C read/write func
+#ifndef i2c_h
+#define i2c_h
+#include "Arduino.h"
 
-#include <Wire.h>
+byte i2c_read(byte device_address, byte register_address);
 
-byte i2c_read(byte device_address, byte register_address)
-{
-  byte data;
-  Wire.beginTransmission(device_address);
-  Wire.write(register_address);
-  Wire.endTransmission(false);
-  Wire.requestFrom(device_address, 1);
-  return Wire.read();
-}
+void i2c_write(byte device_address, byte register_address, byte data);
 
-void i2c_write(byte device_address, byte register_address, byte data)
-{
-  Wire.beginTransmission(device_address);
-  Wire.write(register_address);
-  Wire.write(data);
-  Wire.endTransmission(false);
-}
+#endif
