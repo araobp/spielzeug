2b70505d81f31dd901a83e683648b797ded366d2
arao <araobp@users.noreply.github.com>
2016-11-30 08:52:16 +0900

    Create lan.h

---
+++ esp_wroom_02/lan.h
@@ -0,0 +1,9 @@
+// Note: this header file is tentative
+
+// WiFi setup
+const char* WIFI_SSID = "****";
+const char* PASSWORD = "****";
+
+// MQTT server
+const char* MQTT_SERVER = "192.168.***.***";
+const int MQTT_PORT = 1883;
