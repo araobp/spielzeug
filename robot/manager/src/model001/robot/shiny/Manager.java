13d9503773bc095ae51f9562101119d9265935d8
arao <alexanderplatz1999@gmail.com>
2016-12-11 08:22:18 +0900

    update along with Google's style guide

--- robot/manager/src/model001/robot/shiny/Manager.java
+++ robot/manager/src/model001/robot/shiny/Manager.java
@@ -3,12 +3,12 @@ package model001.robot.shiny;
 import java.util.Scanner;
 
 public class Manager {
-
-	public static void main(String args[]) {
-		Scanner scanner = new Scanner(System.in);
-		String in = scanner.next();
-		
-		System.out.println(in);
-		scanner.close();
-	}
+      
+  public static void main(String args[]) {
+    Scanner scanner = new Scanner(System.in);
+    String in = scanner.next();
+    
+    System.out.println(in);
+    scanner.close();
+  }
 }
