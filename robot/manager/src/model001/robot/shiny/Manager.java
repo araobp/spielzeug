18e320838ad73ad771c9e3d4538eed9294efc86b
arao <alexanderplatz1999@gmail.com>
2016-12-11 08:12:29 +0900

    code tree change

---
+++ robot/manager/src/model001/robot/shiny/Manager.java
@@ -0,0 +1,14 @@
+package model001.robot.shiny;
+
+import java.util.Scanner;
+
+public class Manager {
+
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		String in = scanner.next();
+		
+		System.out.println(in);
+		scanner.close();
+	}
+}
