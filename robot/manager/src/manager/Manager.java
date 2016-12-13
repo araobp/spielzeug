82591a10833d2604e529fa61ed5477aa910c72b0
arao <alexanderplatz1999@gmail.com>
2016-12-14 00:30:32 +0900

    Test commit

---
+++ robot/manager/src/manager/Manager.java
@@ -0,0 +1,17 @@
+package manager;
+
+import java.util.Scanner;
+
+public class Manager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+	    Scanner scanner = new Scanner(System.in);
+	    String in = scanner.next();
+	        
+	    System.out.println(in);
+	    scanner.close();
+	}
+
+}
